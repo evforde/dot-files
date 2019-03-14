f01f2ac20dbe9b36079407e0f3e47a8d25099ab4
Elliott Forde <elliott.forde@robinhood.com>
2019-03-14 16:25:59 -0700

    [vim_setup] Add salt highlighting

--- vim_setup.sh
+++ vim_setup.sh
@@ -16,6 +16,7 @@ git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 git clone https://github.com/tpope/vim-rhubarb.git ~/.vim/bundle/vim-rhubarb
+git clone https://github.com/saltstack/salt-vim.git ~/.vim/bundle/salt-vim
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
