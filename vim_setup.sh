3d36d6e552fa6c5e86b0fda0f33e169017756ad2
Elliott Forde <elliott.forde@robinhood.com>
2019-03-15 10:24:22 -0700

    [vim_setup] Organize and install vim from source

--- vim_setup.sh
+++ vim_setup.sh
@@ -2,21 +2,40 @@
 mkdir -p ~/.vim/colors
 cp ./molokai.vim ~/.vim/colors/molokai.vim
 
+
+# install vim from source
+cd ~
+git clone https://github.com/vim/vim.git
+cd vim
+./configure --with-features=huge --disable-pythoninterp --enable-python3interp vi_cv_path_python3=/usr/local/bin/python3.6 && make && sudo make install
+
+
+# TODO navigate these to vimrc
+# file navigation 
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/tpope/vim-vinegar.git ~/.vim/bundle/vim-vinegar
 git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 
+# status line
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
 git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+
+# coding display
 git clone https://github.com/Yggdroot/indentLine.git ~/.vim/bundle/indentLine
-git clone https://github.com/tpope/vim-obsession.git ~/.vim/bundle/vim-obsession
 git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/supertab
-git clone https://github.com/benmills/vimux.git ~/.vim/bundle/vimux
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/saltstack/salt-vim.git ~/.vim/bundle/salt-vim
+
+# sessions
+git clone https://github.com/tpope/vim-obsession.git ~/.vim/bundle/vim-obsession
+
+# git
 git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 git clone https://github.com/tpope/vim-rhubarb.git ~/.vim/bundle/vim-rhubarb
-git clone https://github.com/saltstack/salt-vim.git ~/.vim/bundle/salt-vim
+
+# tmux stuff
+git clone https://github.com/benmills/vimux.git ~/.vim/bundle/vimux
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
