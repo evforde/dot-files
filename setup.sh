04e9c44c23856cf2845bcd6b7b5805d37499c5af
Elliott Forde <elliott@mac.lan>
2025-06-25 11:19:45 -0700

    Improve setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,13 @@ git config --global user.name "Elliott Forde"
 
 # iterm settings
 mkdir -p ~/Library/Application\ Support/iTerm2/Scripts/Auto-Launch/
-ln ~/.dot-files/iterm/folder_title.py ~/Library/Application\ Support/iTerm2/Scripts/Auto-Launch/folder_title.py
+ln ~/dot-files/iterm/folder_title.py ~/Library/Application\ Support/iTerm2/Scripts/Auto-Launch/folder_title.py
+
+ln -s ~/dot-files/.bash_profile ~/.bash_profile
+ln -s ~/dot-files/.vimrc ~/.vimrc
+ln -s ~/dot-files/.vimrc_vscode ~/.vimrc_vscode
+ln -s ~/dot-files/.zshrc ~/.zshrc
+ln -s ~/dot-files/jj-config.toml ~/.config/jj/config.toml
 
 echo ""
 echo "Don't forget to set your GitHub email!"
